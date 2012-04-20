05407269082a677be9b7fbde95090beacb8cc5fb
Justin Garten <jgarten@gmail.com>
2012-04-20 16:12:27 -0700

    Updates to allow variable grids and focused learning

---
+++ qLearning/QLiveBotFocused.java
@@ -0,0 +1,250 @@
+package qLearning;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.HitByBulletEvent;
+import java.awt.Graphics2D;
+
+//Implements Q-Learning with the start state set to your current location rather than trying to determine a more general policy (with random start points).
+public class QLiveBotFocused extends Robot {
+	//Constants for orientation
+	private final double NORTH = 0.0;
+	private final double NORTH_ALT = 360.0;
+	private final double SOUTH = 180.0;
+	private final double EAST = 90.0;
+	private final double WEST = 270.0;
+	private final double NORTHWEST = 315.0;
+	private final double NORTHEAST = 45.0;
+	private final double SOUTHWEST = 225.0;
+	private final double SOUTHEAST = 135.0;
+	private final int MAX_BLIND = 4;
+	//Policy
+    private int[] policy = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+    public boolean currently_updating;
+    private int goal_state;
+    private int last_view = 0;
+    
+;	public void run() {
+		currently_updating = false;
+    	 while (true) {
+    		 last_view = last_view + 1;
+    		 if (last_view > MAX_BLIND) {
+    			 updatePolicy();
+    		 }
+        	//Each time we get a turn, we find out the state we are in and execute the action our policy tells us to
+        	int state = QUtilities.XYtoState(getX(), getY());
+        	if (policy[state] == QUtilities.ACTION_NORTH) {
+        		goNorth(10);
+        	} else if (policy[state] == QUtilities.ACTION_SOUTH) {
+        		goSouth(10);
+        	} else if (policy[state] == QUtilities.ACTION_EAST) {
+        		goEast(10);
+        	} else if (policy[state] == QUtilities.ACTION_WEST) {
+        		goWest(10);
+        	} else if (policy[state] == QUtilities.ACTION_NORTHWEST) {
+        		goNorthwest(10);
+        	} else if (policy[state] == QUtilities.ACTION_NORTHEAST) {
+        		goNortheast(10);
+        	} else if (policy[state] == QUtilities.ACTION_SOUTHWEST) {
+        		goSouthwest(10);
+        	} else if (policy[state] == QUtilities.ACTION_SOUTHEAST) {
+        		goSoutheast(10);
+        	} else if (policy[state] == -1) {
+            	double r = Math.random();
+            	if (r < 0.125) {
+            		goNorth(10);
+            	} else if (r >= 0.125 && r < 0.25) {
+            		goSouth(100);
+            	} else if (r >= 0.25 && r < 0.375) {
+            		goEast(100);
+            	} else if (r >= 0.375 && r < 0.5) {
+            		goWest(100);
+            	} else if (r >= 0.5 && r < 0.625) {
+            		goNorthwest(100);
+            	} else if (r >= 0.625 && r < 0.750) {
+            		goSouthwest(100);
+            	} else if (r >=0.75 && r < 0.875) {
+            		goSoutheast(100);
+            	} else if (r >= 0.875 && r < 1.0) {
+            		goNortheast(100);
+            	}
+        	}
+        }
+}
+
+    /*
+     * Fire when we scan a robot. Our policy keeps us oriented toward the enemy almost all the time, so firing straight ahead works, so
+     * long as we keep our gun heading the same as our body heading. http://old.nabble.com/Using-Random-Statements-td4010734.html
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+    	last_view = 0;
+    	double enemyBearing = getHeading() + e.getBearing(); 
+    	double enemyX = getX() + e.getDistance() * Math.sin(Math.toRadians(enemyBearing)); 
+    	double enemyY = getY() + e.getDistance() * Math.cos(Math.toRadians(enemyBearing));
+    	double time_to_target = e.getDistance() / 2.0;
+    	double est_update_time = 3.0;
+    	double x = enemyX + e.getVelocity() * (est_update_time + time_to_target) * Math.sin(Math.toRadians(e.getHeading()));
+        double y = enemyY + e.getVelocity() * (est_update_time + time_to_target) * Math.cos(Math.toRadians(e.getHeading()));
+    	System.out.print("Found enemy at: (" + enemyX + "," + enemyY + ")\n" );
+    	goal_state = QUtilities.XYtoState(enemyX, enemyY);
+    	updatePolicy();
+        fire(1);
+	}
+    
+    public void updatePolicy() {
+    	if (!currently_updating) {
+    		currently_updating = true;
+    		System.out.print("Updating the policy\n");
+    		Thread policy_update = new Thread() {
+    			public void run() {
+    					policy = QUtilities.QtoPolicy(QUtilities.generateQTable(goal_state, QUtilities.XYtoState(getX(), getY())));
+    					doneUpdating();
+			    	}
+				};
+			policy_update.start();
+    	}
+    }
+
+	public void doneUpdating() {
+		currently_updating = false;
+	}
+	
+	public void onHitByBullet(HitByBulletEvent e) {
+        
+	}
+
+	/*
+	 * This just prints out some information on the Robocode battlefield itself. Useful for debugging purposes.
+	 * Shows you the state, heading, gun heading, energy, radar heading, x position, and y position of the 
+	 * robot. This is how we verified our motion model and tiling function primarily.
+	 */
+	public void onPaint(Graphics2D g) {
+    	// Set the paint color to red
+    	g.setColor(java.awt.Color.WHITE);
+    	// Paint a filled rectangle at (50,50) at size 100x150 pixels
+    	int state = QUtilities.XYtoState(getX(), getY());
+    	String state_str = Integer.toString(state);
+    	String xpos_str = Double.toString(getX());
+    	String ypos_str = Double.toString(getY());
+    	String heading = Double.toString(getHeading());
+    	String gunheading = Double.toString(getGunHeading());
+    	String energy = Double.toString(getEnergy());
+    	String radarheading = Double.toString(getRadarHeading());
+    	energy = "Energy: " + energy;
+    	xpos_str = "X: " + xpos_str;
+    	ypos_str = "Y: " + ypos_str;
+    	heading = "Heading: " + heading;
+    	gunheading = "Gun Heading: " + gunheading;
+    	radarheading = "Radar Heading: " + radarheading;
+    	state_str = "State: " + state_str;
+    	char[] xpos = xpos_str.toCharArray();
+    	char[] ypos = ypos_str.toCharArray();
+    	char[] headingchars = heading.toCharArray();
+    	char[] gunheadingchars = gunheading.toCharArray();
+    	char[] energychars = energy.toCharArray();
+    	char[] radarheadingchars = radarheading.toCharArray();
+    	char[] statechars = state_str.toCharArray();
+    	g.drawChars(statechars, 0, statechars.length, 250, 125);
+    	g.drawChars(radarheadingchars, 0, radarheadingchars.length, 250, 110);
+    	g.drawChars(energychars, 0, energychars.length, 250, 95);
+    	g.drawChars(gunheadingchars, 0, gunheadingchars.length, 250, 80);
+    	g.drawChars(headingchars, 0, headingchars.length, 250, 65);
+    	g.drawChars(xpos, 0, xpos.length, 250, 50);
+    	g.drawChars(ypos, 0, ypos.length, 250, 35);
+	}
+	
+	//Action to turn and head north from any initial orientation
+	public void goNorth(int distance) {
+		double current_heading = getHeading();
+		if (current_heading <= SOUTH) {
+			turnLeft(current_heading - NORTH);
+		} else {
+			turnRight(NORTH_ALT - current_heading);
+		}
+		ahead(distance);
+	}
+	
+	//Action to turn and head south from any initial orientation
+	public void goSouth(int distance) {
+		double current_heading = getHeading();
+		if (current_heading <= SOUTH) {
+			turnRight(SOUTH - current_heading);
+		} else {
+			turnLeft(current_heading - SOUTH);
+		}
+		ahead(distance);
+	}
+	
+	//Action to turn and head east from any initial orientation
+	public void goEast(int distance) {
+		double current_heading = getHeading();
+		if (current_heading >= WEST || current_heading < EAST) {
+			if (current_heading < 360.0 && current_heading >= WEST) turnRight(NORTH_ALT - current_heading + EAST);
+			else turnRight(EAST - current_heading);
+		} else {
+			turnLeft(current_heading - EAST);
+		}
+		ahead(distance);
+	}
+	
+	//Action to turn and head west from any initial orientation
+	public void goWest(int distance) {
+		double current_heading = getHeading();
+		if (current_heading > WEST || current_heading <= EAST) {
+			if (current_heading < 360.0 && current_heading >= WEST) turnLeft(current_heading - WEST);
+			else turnLeft(NORTH_ALT - WEST + current_heading);
+		} else {
+			turnRight(WEST - current_heading);
+		}
+		ahead(distance);
+	}
+	
+	//Action to turn and head northwest from any initial orientation
+	public void goNorthwest(int distance) {
+		double current_heading = getHeading();
+		if (current_heading > NORTHWEST || current_heading <= SOUTHEAST) {
+			if (current_heading < 360.0 && current_heading >= NORTHWEST) turnLeft(current_heading - NORTHWEST);
+			else turnLeft(NORTH_ALT - NORTHWEST + current_heading);
+		} else {
+			turnRight(NORTHWEST - current_heading);
+		}
+		ahead(distance);
+	}
+	
+	//Action to turn and head northwest from any initial orientation
+	public void goSouthwest(int distance) {
+		double current_heading = getHeading();
+		if (current_heading >= NORTHEAST && current_heading < SOUTHWEST) {
+			turnRight(SOUTHWEST - current_heading);
+		} else {
+			if (current_heading < 360.0 && current_heading >= SOUTHWEST) turnLeft(current_heading - SOUTHWEST);
+			else turnLeft(current_heading + NORTH_ALT - SOUTHWEST);
+		}
+		ahead(distance);
+	}
+	
+	//Action to turn and head northeast from any initial orientation
+	public void goNortheast(int distance) {
+		double current_heading = getHeading();
+		if (current_heading >= NORTHEAST && current_heading < SOUTHWEST) {
+			turnLeft(current_heading - NORTHEAST);
+		} else {
+			if (current_heading < 360.0 && current_heading >= SOUTHWEST) turnRight(NORTH_ALT - current_heading + NORTHEAST);
+			else turnRight(NORTHEAST - current_heading);
+		}
+		ahead(distance);
+	}
+	
+	//Action to turn and head southeast from any initial orientation
+	public void goSoutheast(int distance) {
+		double current_heading = getHeading();
+		if (current_heading > NORTHWEST || current_heading < SOUTHEAST) {
+			if (current_heading < 360.0 && current_heading >= NORTHWEST) turnRight(NORTH_ALT - current_heading + SOUTHEAST);
+			else turnRight(SOUTHEAST - current_heading);
+		} else {
+			turnLeft(current_heading - SOUTHEAST);
+		}
+		ahead(distance);
+	}
+}
+
