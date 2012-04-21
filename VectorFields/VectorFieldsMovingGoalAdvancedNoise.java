3d9946e91884b5c965de4cb9ae3c357cdd927ca2
may ang <mayday815@gmail.com>
2012-04-20 18:13:10 -0700

    Noise everywhere

---
+++ VectorFields/VectorFieldsMovingGoalAdvancedNoise.java
@@ -0,0 +1,142 @@
+package VectorFields;
+
+import robocode.*;
+import java.lang.*;
+
+/**
+ * A Robocode robot that attempts to use vector fields to navigate towards a moving goal. This
+ * uses an advanced robot in an effort to smooth out the motion issues with the simple robots.
+ */
+public class VectorFieldsMovingGoalAdvancedNoise extends AdvancedRobot
+{
+    private static final int SCREEN_WIDTH = 600;
+    private static final int SCREEN_HEIGHT = 600;
+    private static final double MAX_DISTANCE = Math.sqrt(Math.pow(SCREEN_WIDTH, 2) + Math.pow(SCREEN_HEIGHT, 2));
+    private static final int MAX_SPEED = 20;
+    private static final int GOAL_DISTANCE = 100;
+
+    private boolean foundGoal = false;
+    private double goalX, goalY;
+
+    //setAdjustGunForRobotTurn(false);
+    //setAdjustRadarForGunTurn(false);
+    //setAdjustRadarForRobotTurn(false);
+
+    public void run()
+    {
+        double angleToGoal, adjustment;
+        double robotX, robotY, heading;
+
+        while (true)
+        {
+            if (foundGoal)
+            {
+                robotX = getX();
+                robotY = getY();
+                // Robocode headings return 0 degrees as North, 90 as East, etc
+                heading = 360 - (getHeading() - 90);
+                angleToGoal = Math.toDegrees(Math.atan2(goalY - robotY, goalX - robotX));
+
+                if (angleToGoal < 0)
+                {
+                    angleToGoal += 360;
+                }
+
+                adjustment = angleToGoal - heading;
+
+                if (adjustment >= 360)
+                {
+                    adjustment -= 360;
+                }
+                else if (adjustment <= -360)
+                {
+                    adjustment += 360;
+                }
+
+                //if (adjustment > 180 || adjustment < -180)
+                    //System.out.println("Out of bounds value: " + adjustment);
+
+                if (adjustment < -180)
+                {
+                    adjustment += 360;
+                }
+                else if (adjustment > 180)
+                {
+                    adjustment -= 360;
+                }
+
+                setTurnRadarLeft(180);
+				adjustment = addNoise(adjustment);
+                setTurnLeft(adjustment);
+                ahead(calcRobotSpeedLinear(robotX, robotY, goalX, goalY));
+                //setTurnRadarLeft(180);
+            }
+            else
+            {
+                //setAdjustRadarForGunTurn(false);
+                //setTurnGunRight(90);
+                //setAdjustRadarForGunTurn(true);
+                setTurnRadarRight(180);
+                //setAdjustRadarForGunTurn(false);
+            }
+
+            execute();
+        }
+    }
+
+    /** 
+     * On scanning any other robot, set it as the current goal.
+     */
+    public void onScannedRobot(ScannedRobotEvent e)
+    {
+        double enemyBearing = getHeading() + e.getBearing();
+        double enemyX = getX() + e.getDistance() * Math.sin(Math.toRadians(enemyBearing));
+        double enemyY = getY() + e.getDistance() * Math.cos(Math.toRadians(enemyBearing));
+
+        foundGoal = true;
+        goalX = enemyX;
+        goalY = enemyY;
+    }
+
+    /**
+     * Linearly decay the speed of the robot when it nears the goal.
+     */
+    public int calcRobotSpeedLinear(double robotX, double robotY, double goalX, double goalY)
+    {
+        int speed = 0;
+
+        double distance = Math.sqrt(Math.pow(robotX - goalX, 2) + Math.pow(robotY - goalY, 2));
+        if (distance >= GOAL_DISTANCE)
+        {
+            speed = MAX_SPEED;
+        }
+        else
+        {
+            speed = (int)((distance / GOAL_DISTANCE) * MAX_SPEED + 0.5);
+        }
+        
+        return speed;
+    }
+
+    /**
+     * Decay the speed "globally." The goal doens't have a range at which it starts to cause a speed drop off.
+     * Instead, the entire screen has a scaled gradient.
+     */
+    public int calcRobotSpeedGlobalFields(double robotX, double robotY, double goalX, double goalY)
+    {
+        double distance = Math.sqrt(Math.pow(robotX - goalX, 2) + Math.pow(robotY - goalY, 2));
+        return (int)((distance / MAX_DISTANCE) * MAX_SPEED + 0.5);
+    }
+	
+	public double addNoise(double adj) {
+		double rand = Math.random();
+		if (rand < 0.2 && rand >= 0.1) {
+			System.out.println("Slip!");
+			adj += 30;
+		} else if (rand < 0.1 && rand >= 0.0) {
+		System.out.println("Slip!");
+			adj -= 30;
+		}
+		return adj;
+	}
+}
---
+++ VectorFields/VectorFieldsObsAvoidMovingNoise.java
@@ -0,0 +1,182 @@
+package VectorFields;
+
+import robocode.*;
+import java.lang.*;
+
+/**
+ * Robocode robot that locates and navigates toward a goal while also avoiding obstacles that it gets near.
+ */
+public class VectorFieldsObsAvoidMovingNoise extends AdvancedRobot
+{
+    private static final int SCREEN_WIDTH = 600;
+    private static final int SCREEN_HEIGHT = 600;
+    private static final double MAX_DISTANCE = Math.sqrt(Math.pow(SCREEN_WIDTH, 2) + Math.pow(SCREEN_HEIGHT, 2));
+    private static final int MAX_SPEED = 20;
+    private static final int GOAL_DISTANCE = 50;
+    private static final int OBJ_DISTANCE = 150;
+
+    private double goalX, goalY;
+    private double obsX, obsY;
+    private boolean foundGoal = false;
+    private boolean foundObstacle = false;
+
+    public void run()
+    {
+        setTurnRadarRight(Double.POSITIVE_INFINITY);
+
+        double robotX, robotY;
+        double robotHeading, angleToGoal, angleToObj;
+        double adjustment;
+        double obsAngle, obsAdjustment;
+        double angleDiff;
+
+        double speedToGoal, speedFromObj;
+
+        while (true)
+        {
+            if (foundGoal)
+            {
+                robotX = getX();
+                robotY = getY();
+
+                robotHeading = 360 - (getHeading() - 90);
+                angleToGoal = Math.toDegrees(Math.atan2(goalY - robotY, goalX - robotX));
+                if (angleToGoal < 0)
+                {
+                    angleToGoal += 360;
+                }
+
+                adjustment = angleToGoal - robotHeading;
+                adjustment = normalizeAngle(adjustment);
+				adjustment = addNoise(adjustment);
+                speedToGoal = calcRobotSpeedLinear(robotX, robotY, goalX, goalY);
+
+                if (foundObstacle)
+                {
+                    speedFromObj = calcObjRepulseSpeed(robotX, robotY, obsX, obsY);
+
+                    if (speedFromObj != 0)
+                    {
+                        obsAngle = Math.toDegrees(Math.atan2(robotY - obsY, robotX - obsX));
+                        if (obsAngle < 0)
+                            obsAngle += 360;
+
+                        angleDiff = obsAngle - angleToGoal;
+                        angleDiff = normalizeAngle(angleDiff);
+                        adjustment += (angleDiff * (speedFromObj / speedToGoal));
+						adjustment = addNoise(adjustment);
+                        speedToGoal -= speedFromObj;
+                    }
+                }
+
+                adjustment = normalizeAngle(adjustment); // Not 100% sure this is needed;
+                setTurnLeft(adjustment);
+                ahead(speedToGoal);
+            }
+
+            execute();
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e)
+    {
+        double targetBearing = getHeading() + e.getBearing();
+        double tmpX = getX() + e.getDistance() * Math.sin(Math.toRadians(targetBearing));
+        double tmpY = getY() + e.getDistance() * Math.cos(Math.toRadians(targetBearing));
+
+        //if (e.getName().equals("VectorFields.VectorFieldsGoal*") && !foundGoal)
+        if (e.getName().equals("VectorFields.VectorFieldsMovingGoal*"))
+        {
+            foundGoal = true;
+            goalX = tmpX;
+            goalY = tmpY;
+        }
+        else if (e.getName().equals("sample.SittingDuck") && !foundObstacle)
+        {
+            System.out.println("Obstacle at " + tmpX + " " + tmpY);
+            foundObstacle = true;
+            obsX = tmpX;
+            obsY = tmpY;
+        }
+
+        setTurnRadarRight(getRadarTurnRemaining());
+    }
+
+    /**
+     * Linearly decay the speed of the robot when it nears the goal.
+     */
+    public double calcRobotSpeedLinear(double robotX, double robotY, double goalX, double goalY)
+    {
+        double speed = 0;
+
+        double distance = Math.sqrt(Math.pow(robotX - goalX, 2) + Math.pow(robotY - goalY, 2));
+        if (distance >= GOAL_DISTANCE)
+        {
+            speed = MAX_SPEED;
+        }
+        else
+        {
+            speed = (distance / GOAL_DISTANCE) * MAX_SPEED + 0.5;
+        }
+        
+        return speed;
+    }
+
+    /**
+     * Decay the speed "globally." The goal doens't have a range at which it starts to cause a speed drop off.
+     * Instead, the entire screen has a scaled gradient.
+     */
+    public int calcRobotSpeedGlobalFields(double robotX, double robotY, double goalX, double goalY)
+    {
+        double distance = Math.sqrt(Math.pow(robotX - goalX, 2) + Math.pow(robotY - goalY, 2));
+        return (int)((distance / MAX_DISTANCE) * MAX_SPEED + 0.5);
+    }
+
+    public double normalizeAngle(double angle)
+    {
+        if (angle <= -360)
+        {
+            angle += 360;
+        }
+        else if (angle >= 360)
+        {
+            angle -= 360;
+        }
+
+        if (angle < -180)
+        {
+            angle += 360;
+        }
+        else if (angle > 180)
+        {
+            angle -= 360;
+        }
+
+        return angle;
+    }
+
+    public double calcObjRepulseSpeed(double robotX, double robotY, double obsX, double obsY)
+    {
+        double speed = 0.0;
+        double distance = Math.sqrt(Math.pow(robotX - obsX, 2) + Math.pow(robotY - obsY, 2));
+
+        if (distance <= OBJ_DISTANCE)
+        {
+            speed = ((OBJ_DISTANCE - distance) / OBJ_DISTANCE) * MAX_SPEED;
+        }
+
+        return speed;
+    }
+	
+	public double addNoise(double adj) {
+		double rand = Math.random();
+		if (rand < 0.2 && rand >= 0.1) {
+			System.out.println("Slip!");
+			adj += 30;
+		} else if (rand < 0.1 && rand >= 0.0) {
+		System.out.println("Slip!");
+			adj -= 30;
+		}
+		return adj;
+	}
+}
